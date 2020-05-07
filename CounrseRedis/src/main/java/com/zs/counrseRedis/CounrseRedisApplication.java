59966df204d218c11afb7a376090d3574b2909de
MroZ11 <zhoushu1118@qq.com>
2020-05-07 11:15:03 +0800

    修改了项目配置文件

---
+++ CounrseRedis/src/main/java/com/zs/counrseRedis/CounrseRedisApplication.java
@@ -0,0 +1,13 @@
+package com.zs.counrseRedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounrseRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounrseRedisApplication.class, args);
+	}
+
+}
